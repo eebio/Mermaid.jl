9beff1bdb38c7bf4008ca09fc52dc1732d5c79ce
Matt J Owen <matt.owen@bristol.ac.uk>
2025-05-15 12:37:02 +0100

    PDE test

---
+++ test/pde.jl
@@ -0,0 +1,102 @@
+@testitem "pde" begin
+    using DifferentialEquations, ModelingToolkit, MethodOfLines, DomainSets
+    # Parameters, variables, and derivatives
+    @parameters t x
+    @variables u(..) g(..) [irreducible = true]
+    Dt = Differential(t)
+    Dx = Differential(x)
+    Dxx = Differential(x)^2
+
+    # 1D PDE and boundary conditions
+    eq = [Dt(u(t, x)) ~ Dxx(u(t, x)) + g(t, x),
+        Dt(g(t, x)) ~ 1]
+    bcs = [u(0, x) ~ sin(pi * x),
+        u(t, 0) ~ 0,
+        u(t, 1) ~ 0,
+        g(0, x) ~ 0.5,
+        Dx(g(t, 0)) ~ 0,
+        Dx(g(t, 1)) ~ 0]
+
+    # Space and time domains
+    domains = [t ∈ Interval(0.0, 1.0),
+        x ∈ Interval(0.0, 1.0)]
+
+    # PDE system
+    @named pdesys = PDESystem(eq, bcs, domains, [t, x], [g(t, x), u(t, x)])
+
+    dx = 0.1
+    # Method of lines discretization
+    discretization = MOLFiniteDifference([x => dx], t)
+
+    # Convert the PDE problem into an ODE problem
+    prob = discretize(pdesys, discretization)
+
+    solPDE = solve(prob, Euler(), dt=0.0001, adaptive=false)
+
+    # Parameters, variables, and derivatives
+    # 1D PDE and boundary conditions
+    eq = [Dt(u(t, x)) ~ Dxx(u(t, x)) + g(t, x),
+        Dt(g(t, x)) ~ 0]
+    bcs = [u(0, x) ~ sin(pi * x),
+        u(t, 0) ~ 0,
+        u(t, 1) ~ 0,
+        g(0, x) ~ 0,
+        Dx(g(t, 0)) ~ 0,
+        Dx(g(t, 1)) ~ 0]
+
+    # Space and time domains
+    domains = [t ∈ Interval(0.0, 1.0),
+        x ∈ Interval(0.0, 1.0)]
+
+    # PDE system
+    @named pdesys = PDESystem(eq, bcs, domains, [t, x], [g(t, x), u(t, x)])
+
+    dx = 0.1
+    # Method of lines discretization
+    discretization = MOLFiniteDifference([x => dx], t)
+
+    # Convert the PDE problem into an ODE problem
+    prob = discretize(pdesys, discretization)
+
+    using SymbolicIndexingInterface
+    function var_index(s)
+        return s => variable_index(prob, ModelingToolkit.parse_variable(prob.f.sys, s))
+    end
+
+    c1 = PDEComponent(
+        model=prob,
+        name="PDE",
+        state_names=Dict(
+            [var_index(s) for s in [["u[$i]" for i in 2:10]... ["g[$i]" for i in 2:10]...]]),
+        time_step=0.0001,
+        alg=Euler(),
+        intkwargs=(:adaptive => false,),
+    )
+
+    function f2(u, p, t)
+        return 1
+    end
+    u0 = 0.5
+    tspan = (0.0, 1.0)
+    prob = ODEProblem(f2, u0, tspan)
+    c2 = ODEComponent(
+        model=prob,
+        name="G",
+        time_step=0.0001,
+        state_names=Dict("g" => 1),
+        alg=Euler(),
+        intkwargs=(:adaptive => false,),
+    )
+
+    conn = Connector(
+        inputs=["G.g"],
+        outputs=["PDE.g[2]", "PDE.g[3]", "PDE.g[4]", "PDE.g[5]", "PDE.g[6]", "PDE.g[7]", "PDE.g[8]", "PDE.g[9]", "PDE.g[10]"],
+    )
+
+    mp = MermaidProblem(components=[c1, c2], connectors=[conn1], max_t=1.0)
+    sol = solve(mp, MinimumTimeStepper())
+
+    finalsol = [0, sol.u["PDE.u[2]"][end], sol.u["PDE.u[3]"][end], sol.u["PDE.u[4]"][end], sol.u["PDE.u[5]"][end], sol.u["PDE.u[6]"][end], sol.u["PDE.u[7]"][end], sol.u["PDE.u[8]"][end], sol.u["PDE.u[9]"][end], sol.u["PDE.u[10]"][end], 0]
+
+    @test all(isapprox.(finalsol, solPDE[u(t, x)][end, :], atol=1e-8))
+end
