4b88a10a2bf01051baa677e9fb1a323331504fbb
Matt J Owen <matt.owen@bristol.ac.uk>
2026-01-14 11:30:53 +0000

    added surrogate test that doesn't use NN

--- test/surrogates.jl
+++ test/surrogates.jl
@@ -89,7 +89,54 @@ end
     # Step both integrators and compare
     n_steps = 20
     tol = 0.1  # Looser tolerances to save training time in tests
+    @test surrogate_int.surrogate isa NeuralSurrogate
+    for i in 1:n_steps
+        step!(ode_int)
+        step!(surrogate_int)
+        orig = getstate(ode_int, ConnectedVariable("decay.x"))
+        sur = getstate(surrogate_int, ConnectedVariable("decay.x"))
+        @test isapprox(sur, orig; atol = tol)
+    end
+end
 
+@testitem "surrogate integrator non-neural" begin
+    using OrdinaryDiffEq
+    using Surrogates
+    using Random
+    Random.seed!(0)
+    # Define a simple ODE: dx/dt = -x, x(0) = 1
+    f(u, p, t) = [-u[1], u[2] - u[1]]
+    u0 = [1.0, 0.5]
+    tspan = (0.0, 1.0)
+    prob = ODEProblem(f, u0, tspan)
+    state_names = OrderedDict("x" => 1)
+    ode_comp = DEComponent(
+        prob, Rodas5(); name = "decay", state_names = state_names, time_step = 0.1)
+
+    # Set bounds for surrogate sampling
+    lower = [0.0, 0.0]
+    upper = [1.0, 1.0]
+
+    # Create surrogate component
+    surrogate_comp = SurrogateComponent(
+        ode_comp,
+        SecondOrderPolynomialSurrogate,
+        lower,
+        upper;
+        n_samples = 2000,
+    )
+
+    # Dummy connector list (no connections for this test)
+    conns = Connector[]
+
+    # Initialize both components
+    ode_int = init(ode_comp, conns)
+    surrogate_int = init(surrogate_comp, conns)
+
+    # Step both integrators and compare
+    n_steps = 20
+    tol = 0.1  # Looser tolerances to save training time in tests
+    @test surrogate_int.surrogate isa SecondOrderPolynomialSurrogate
     for i in 1:n_steps
         step!(ode_int)
         step!(surrogate_int)
